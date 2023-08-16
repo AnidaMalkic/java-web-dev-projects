0417d661ca0ee908556d2acda72da5daa02e84cd
AnidaMalkic <malkic.anida@gmail.com>
2023-08-16 09:54:52 -0500

    second commit

--- control-flow-and-collections/exercises/collections-exercises/src/main/java/org/example/Main.java
+++ control-flow-and-collections/exercises/collections-exercises/src/main/java/org/example/Main.java
@@ -1,10 +1,16 @@
 package org.example;
 
+import javax.print.attribute.IntegerSyntax;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Scanner;
+
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
@@ -19,7 +25,6 @@ public class Main {
         }
 
 
-
         String phrase = " I would not, could not, in a box. I would not, could not with a fox. I will not eat them in a house. I will not eat them with a mouse.";
         String[] words = phrase.split(" ");
         System.out.println(Arrays.toString(words));
@@ -28,6 +33,24 @@ public class Main {
         String[] sentences = phrase.split("\\.");
         System.out.println(Arrays.toString(sentences));
 
+
+        ArrayList<Integer> digits = new ArrayList<Integer>(
+                Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13)
+        );
+
+
+        System.out.println(sumEven(digits));
     }
 
+
+    public static int sumEven(ArrayList<Integer> arr) {
+        int total = 0;
+        for (int integer : arr) {
+            if (integer % 2 == 0) {
+                total += integer;
+            }
+        }
+        return total;
+    }
 }
+
