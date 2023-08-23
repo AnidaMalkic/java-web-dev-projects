d7ca5f27240ab3470ded8f480d603325b85b76de
AnidaMalkic <malkic.anida@gmail.com>
2023-08-23 12:55:43 -0500

    first commit

---
+++ classes-part-one/exercises/src/main/java/Teacher.java
@@ -0,0 +1,39 @@
+public class Teacher {
+    private String firstName;
+    private String lastname;
+    private String subject;
+    private int yearsTeaching;
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public int getYearsTeaching() {
+        return yearsTeaching;
+    }
+
+    public void setFirstName(String aFirstName) {
+        firstName = aFirstName;
+    }
+
+    public void setLastname(String aLastname) {
+        lastname = aLastname;
+    }
+
+    public void setSubject(String aSubject) {
+        subject = aSubject;
+    }
+
+    public void setYearsTeaching(int aYearsTeaching) {
+        yearsTeaching = aYearsTeaching;
+    }
+}
